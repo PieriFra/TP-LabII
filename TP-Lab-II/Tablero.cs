0006be0592720716a51fb68c158d1faac6d4ff3a
Pierina Fraire <pierinafrairr@gmail.com>
2021-10-06 12:18:37 -0300

    Metodo Verificar tablero

--- TP-Lab-II/Tablero.cs
+++ TP-Lab-II/Tablero.cs
@@ -8,5 +8,29 @@ namespace TP_Lab_II
 {
     class Tablero
     {
+        private const int tam = 8;
+        private const int cant_fichas= 9;
+        protected Tablero(int tam_, int cant_fichas_) { tam_ = tam; cant_fichas_ = cant_fichas; }
+        ~Tablero() { }
+
+
+        public bool VerificarTablero(int [,]tableroAux)
+        {
+           //verifica que todas las posciones del tablero esten siendo atacadas
+            for (int i=0; i< tam; i++)
+            {
+                for(int j=0; j<tam;j++)
+                {
+                    //en caso de encontar una posicion que no esta siendo atacada retorna false
+                    if (tableroAux[i, j] == 0) 
+                        return false;
+                }
+            }
+
+            //si recorre todo el tablero y no encuentra ninguna posicion libre, efectivamente todas las posciones
+           //estan siendo atacadas y retorna true
+            return true; 
+        }
     }
 }
+
