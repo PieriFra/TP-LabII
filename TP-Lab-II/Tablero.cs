30e1790172ed542d9e6bd29eb5fc1819c4fe91f8
Pierina Fraire <pierinafrairr@gmail.com>
2021-10-06 15:33:19 -0300

    Funcion CalculoSolucion

--- TP-Lab-II/Tablero.cs
+++ TP-Lab-II/Tablero.cs
@@ -9,8 +9,9 @@ namespace TP_Lab_II
     class Tablero
     {
         private const int tam = 8;
-        private const int cant_fichas= 9;
-        protected Tablero(int tam_, int cant_fichas_) { tam_ = tam; cant_fichas_ = cant_fichas; }
+        private List<Ficha> ListaFichas;
+        private int [,]TableroAux; //debemos inicializar aca??
+        protected Tablero(int tam_, List<Ficha> ListaFichas_) { tam_ = tam; ListaFichas_ = ListaFichas; }
         ~Tablero() { }
 
 
@@ -30,6 +31,40 @@ namespace TP_Lab_II
             //si recorre todo el tablero y no encuentra ninguna posicion libre, efectivamente todas las posciones
            //estan siendo atacadas y retorna true
             return true; 
+        }
+
+        public Tablero CalculoSolucion(Tablero tableroOriginal, int[,] tableroAux, List<Tablero> ListaResultados, int n_tableros)
+        {
+            var rand = new Random(); //verrrrr
+            int mov = 0; //contador de movimientos.
+            do
+            {
+                if (mov > 8)
+                {
+                    for (int i = 0; i < 8; i++)
+                    {
+                        int Ficha_mover = rand.Next(0, 8);
+                        CalcularMovimiento(Ficha_mover, tableroOriginal);
+                        AnalizarTablero(tableroOriginal, tableroAux);
+                        mov++;
+                    }
+                }
+
+                //si en 5 movimientos no encontramos una solución juntamos un caballo y una torre y movemos siempre esa ficha
+                CalcularMovimientos(9, tableroOriginal); //realizamos un movimiento
+                AnalizarTableroAux(tableroOriginal, tableroAux);
+
+            } while (VerificarTablero(tableroAux) == false);
+
+            //verificamos que la solucion no se repita
+            for(int i=0; i < n_tableros; i++)
+            {
+                if (ListaResultados[i] != tableroOriginal)
+                    return tableroOriginal; //si no existía retornamos la nueva solución 
+            }
+
+            //en caso de que ya exista la solucion retornamos null, o que mas podriamos retornar???
+            return null;
         }
     }
 }
